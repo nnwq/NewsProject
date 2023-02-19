02328151d950574498c930f3a4d8567546bab88b
nnwq <dmitrypomishin@gmail.com>
2023-02-19 12:30:00 +0100

    initial commit

--- NewsProject/wsgi.py
+++ NewsProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for NewsProject project.
+WSGI config for NewsAndPosts project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsAndPosts.settings')
 
 application = get_wsgi_application()
---
+++ news/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Author(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Post(models.Model):
+    pass
+
+
+class PostCategory(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
