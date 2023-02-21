d2123f75a86e28ccefb952c205b81c65dc593f98
nnwq <dmitrypomishin@gmail.com>
2023-02-21 13:00:53 +0100

    filled in models

--- NewsProject/asgi.py
+++ NewsProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for NewsAndPosts project.
+ASGI config for NewsProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsAndPosts.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsProject.settings')
 
 application = get_asgi_application()
--- news/models.py
+++ news/models.py
@@ -1,25 +1,72 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
 
 class Author(models.Model):
-    pass
+    name = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        for post in Post.objects.filter(author=self):
+            self.rating += Post.rating*3
+            for comment in Comment.objects.filer(post=post):
+                self.rating += comment.rating
+        for comment in Comment.objects.filter(user=self.name):
+            self.rating += comment.rating
+        self.save()
 
 
 class Category(models.Model):
-    pass
+    category_name = models.CharField(unique=True, max_length=255)
 
 
 class Post(models.Model):
-    pass
+    ARTICLE = 'AR'
+    NEWS = 'NW'
+    POST_CHOICES = [
+        ('AR', 'Article'),
+        ('NW', 'News')
+    ]
+    title = models.CharField(max_length=50, unique=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    type_choice = models.CharField(choices=POST_CHOICES, default='News', max_length=10)
+    time_created = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, through='PostCategory')
+    content = models.TextField(default='No Content')
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating + 1
+        self.save()
+
+    def dislike(self):
+        self.rating - 1
+        self.save()
+
+    def preview(self):
+        self.content = self.content[0:50] + '...'
+        self.save()
 
 
 class PostCategory(models.Model):
-    pass
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
-    pass
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.ForeignKey(Category, on_delete=models.CASCADE)
+    text = models.CharField(default='An Upvote comment', max_length=255)
+    time_created = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
 
+    def like(self):
+        self.rating + 1
+        self.save()
+
+    def dislike(self):
+        self.rating - 1
+        self.save()
 
